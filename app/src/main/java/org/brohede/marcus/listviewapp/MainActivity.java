15b91e567e3e0b3f0a18d90d30966867fd33c9e4
aliceanglesjo <a17alian@student.his.se>
2018-03-29 13:49:22 +0200

    ListView

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,6 +2,12 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // Let the static raw data that you use in your ListView be created here as a
@@ -23,8 +29,18 @@ public class MainActivity extends AppCompatActivity {
         //                      an individual item in the ListView we are creating.
         // Here you should enter your code that fills the ListView
         // 1. Create an array
+
+        String[] rawData = {"Leif", "Ulla", "Kjell"};
         // 2. Create a List object with your array from step 1 as in-data
+        List<String> listData = new ArrayList<String>(Arrays.asList(rawData));
+
         // 3. Create an ArrayAdapter object that connects
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,R.id.my_item_textview,listData);
+
+        ListView myListView = (ListView)findViewById(R.id.my_item_textview);
+        myListView.setAdapter(adapter);
+        adapter.add("Hilding");
+
         //    * list_item_textview
         //    * my_item_textview
         //    * List object created in step 2
